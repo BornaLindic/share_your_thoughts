2b54a77ec876f68a831a043331d3252bee246dd5
Borna Lindić <borna.lindic@fer.hr>
2023-12-16 13:11:56 +0100

    back sync

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# share_your_thoughts
-share_your_thoughts
+Primjeri se pokreću s:
+  * npm run ver01
+  * npm run ver02
+  * ...
+  * npm run ver06
+
+
+VAPID ključevi se generiraju s:
+  * npm run gen-vapid
+
+
+
+Sva pitanja, bugove i prijedloge za poboljšanje javiti na igor.mekterovic@fer.hr
---
+++ public/sw.js
@@ -0,0 +1,111 @@
+import {del, entries} from "https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm";
+const filesToCache = [
+    "/",
+    "manifest.json",
+    "index.html",
+    "offline.html",
+    "404.html",
+];
+
+const staticCacheName = "static-cache";
+
+self.addEventListener("install", (event) => {
+    console.log("Attempting to install service worker and cache static assets");
+    event.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    console.log("Activating new service worker...");
+
+    const cacheWhitelist = [staticCacheName];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then((response) => {
+                if (response) {
+                    console.log("Found " + event.request.url + " in cache!");
+                    return response;
+                }
+                console.log(
+                    "----------------->> Network request for ",
+                    event.request.url
+                );
+                return fetch(event.request).then((response) => {
+                    console.log("response.status = " + response.status);
+                    if (response.status === 404) {
+                        return caches.match("404.html");
+                    }
+                    return caches.open(staticCacheName).then((cache) => {
+                        console.log(">>> Caching: " + event.request.url);
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log("Error", event.request.url, error);
+                return caches.match("offline.html");
+            })
+    );
+});
+
+
+self.addEventListener('sync', function (event) {
+    console.log('Background sync!', event);
+    if (event.tag === 'sync-audio') {
+        event.waitUntil(
+            syncAudio()
+        );
+    }
+});
+
+let syncAudio = async function () {
+    entries()
+        .then((entries) => {
+            entries.forEach((entry) => {
+                let snap = entry[1]; //  Each entry is an array of [key, value].
+                let formData = new FormData();
+                formData.append('id', snap.id);
+                formData.append('ts', snap.ts);
+                formData.append('title', snap.title);
+                formData.append('audio', snap.audio, snap.id + '.wav');
+                fetch('/saveAudio', {
+                        method: 'POST',
+                        body: formData
+                    })
+                    .then(function (res) {
+                        if (res.ok) {
+                            res.json()
+                                .then(function (data) {
+                                    console.log("Deleting from idb:", data.id);
+                                    del(data.id);
+                                });
+                        } else {
+                            console.log(res);
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            })
+        });
+}
+
